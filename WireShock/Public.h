f4f420ebef614cc5ed7b9686765c5d3773c9090e
Benjamin Höglinger <nefarius@dhmx.at>
2018-05-19 19:11:30 +0200

    Add project files.

---
+++ WireShock/Public.h
@@ -0,0 +1,24 @@
+/*++
+
+Module Name:
+
+    public.h
+
+Abstract:
+
+    This module contains the common declarations shared by driver
+    and user applications.
+
+Environment:
+
+    user and kernel
+
+--*/
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_WireShock,
+    0x747b893a,0x709f,0x48ae,0x9a,0x74,0x9c,0x99,0xf1,0x82,0x4f,0x77);
+// {747b893a-709f-48ae-9a74-9c99f1824f77}
