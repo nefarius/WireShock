6960e78912e917ff1edbb907581ce064f366e11e
Benjamin Höglinger <nefarius@dhmx.at>
2018-05-22 21:27:54 +0200

    Ported over Bulkrwr code

--- WireShock/Bulkrwr.c
+++ WireShock/Bulkrwr.c
@@ -105,14 +105,160 @@ WireShockEvtUsbBulkReadPipeReadComplete(
     WDFCONTEXT  Context
 )
 {
+    NTSTATUS                        status;
+    PDEVICE_CONTEXT                 pDeviceContext = Context;
+    PUCHAR                          buffer;
+    BTH_HANDLE                      clientHandle;
+    PBTH_DEVICE                     pClientDevice = NULL;
+    L2CAP_SIGNALLING_COMMAND_CODE   code;
+
+    static BYTE CID = 0x01;
+
     UNREFERENCED_PARAMETER(Pipe);
-    UNREFERENCED_PARAMETER(Buffer);
-    UNREFERENCED_PARAMETER(NumBytesTransferred);
-    UNREFERENCED_PARAMETER(Context);
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_BULKRWR, "%!FUNC! Entry");
+    if (NumBytesTransferred == 0) {
+        TraceEvents(TRACE_LEVEL_WARNING, TRACE_BULKRWR,
+            "!FUNC! Zero length read "
+            "occurred on the Interrupt Pipe's Continuous Reader\n"
+        );
+        return;
+    }
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_BULKRWR, "%!FUNC! Exit");
+    buffer = WdfMemoryGetBuffer(Buffer, NULL);
+
+    BTH_HANDLE_FROM_BUFFER(clientHandle, buffer);
+
+    //pClientDevice = BTH_DEVICE_LIST_GET_BY_HANDLE(&pDeviceContext->ClientDeviceList, &clientHandle);
+
+    if (pClientDevice == NULL)
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, TRACE_BULKRWR, "PBTH_DEVICE not found");
+        return;
+    }
+
+    if (L2CAP_IS_CONTROL_CHANNEL(buffer))
+    {
+        if (L2CAP_IS_SIGNALLING_COMMAND_CODE(buffer))
+        {
+            code = L2CAP_GET_SIGNALLING_COMMAND_CODE(buffer);
+
+            switch (code)
+            {
+            case L2CAP_Command_Reject:
+            {
+                PL2CAP_SIGNALLING_COMMAND_REJECT data = (PL2CAP_SIGNALLING_COMMAND_REJECT)&buffer[8];
+
+                TraceEvents(TRACE_LEVEL_ERROR, TRACE_BULKRWR, ">> L2CAP_Command_Reject: 0x%04X", data->Reason);
+
+                break;
+            }
+
+#pragma region L2CAP_Connection_Request
+
+            case L2CAP_Connection_Request:
+
+                status = Ds3ConnectionRequest(
+                    pDeviceContext,
+                    pClientDevice,
+                    buffer,
+                    &CID);
+
+                break;
+
+#pragma endregion
+
+#pragma region L2CAP_Connection_Response
+
+            case L2CAP_Connection_Response:
+
+                status = Ds3ConnectionResponse(
+                    pDeviceContext,
+                    pClientDevice,
+                    buffer,
+                    &CID);
+
+                break;
+
+#pragma endregion
+
+#pragma region L2CAP_Configuration_Request
+
+            case L2CAP_Configuration_Request:
+
+                status = Ds3ConfigurationRequest(
+                    pDeviceContext,
+                    pClientDevice,
+                    buffer);
+
+                break;
+
+#pragma endregion
+
+#pragma region L2CAP_Configuration_Response
+
+            case L2CAP_Configuration_Response:
+
+                status = Ds3ConfigurationResponse(
+                    pDeviceContext,
+                    pClientDevice,
+                    buffer,
+                    &CID);
+
+                break;
+
+#pragma endregion
+
+#pragma region L2CAP_Disconnection_Request
+
+            case L2CAP_Disconnection_Request:
+
+                status = Ds3DisconnectionRequest(
+                    pDeviceContext,
+                    pClientDevice,
+                    buffer);
+
+                break;
+
+#pragma endregion
+
+#pragma region L2CAP_Disconnection_Response
+
+            case L2CAP_Disconnection_Response:
+
+                status = Ds3DisconnectionResponse(
+                    pDeviceContext,
+                    pClientDevice);
+
+                break;
+
+#pragma endregion
+
+            default:
+                TraceEvents(TRACE_LEVEL_WARNING, TRACE_BULKRWR, "Unknown L2CAP command: 0x%02X", code);
+                break;
+            }
+        }
+    }
+    else if (L2CAP_IS_HID_INPUT_REPORT(buffer))
+    {
+        switch (pClientDevice->DeviceType)
+        {
+        case DualShock3:
+
+            status = Ds3ProcessHidInputReport(pClientDevice, buffer);
+
+            break;
+        default:
+            break;
+        }
+    }
+    else
+    {
+        status = Ds3InitHidReportStage(
+            pDeviceContext,
+            pClientDevice,
+            &CID);
+    }
 }
 
 BOOLEAN
