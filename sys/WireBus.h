4bc58de43908f2e5a52a88604161d35006915e1c
Benjamin Höglinger <nefarius@dhmx.at>
2018-06-28 13:48:13 +0200

    Removed duplicated code Updated common

--- sys/WireBus.h
+++ sys/WireBus.h
@@ -28,15 +28,3 @@ EVT_WDF_CHILD_LIST_CREATE_DEVICE WireShockEvtWdfChildListCreateDevice;
 EVT_WDF_IO_QUEUE_IO_INTERNAL_DEVICE_CONTROL WireChildEvtWdfIoQueueIoInternalDeviceControl;
 EVT_WDF_CHILD_LIST_ADDRESS_DESCRIPTION_CLEANUP WireShockEvtWdfChildListAddressDescriptionCleanup;
 EVT_WDF_TIMER WireChildOutputReportEvtTimerFunc;
-
-VOID FORCEINLINE REVERSE_BYTE_ARRAY(PUCHAR start, int size) 
-{
-    PUCHAR lo = start;
-    PUCHAR hi = start + size - 1;
-    UCHAR swap;
-    while (lo < hi) {
-        swap = *lo;
-        *lo++ = *hi;
-        *hi-- = swap;
-    }
-}
